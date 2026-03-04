7cb687fa6f7a7b0a3d2a4e0d1da368582d527bee
conmeobeo0707 <Conmeo07@>
2026-03-04 17:35:26 +0700

    fix login

--- Assets/Scripts/Login/RegisterController.cs
+++ Assets/Scripts/Login/RegisterController.cs
@@ -67,7 +67,7 @@ public class RegisterController : MonoBehaviour
     IEnumerator RegisterCoroutine(string username, string password)
     {
         //string url = "https://localhost:7068/api/Authentication/register";
-        string url = "https://p0fbncb4-7068.asse.devtunnels.ms/api/Authentication/register";
+        string url = "https://winearthserver.onrender.com/api/Authentication/register";
 
         RegisterRequest data = new RegisterRequest
         {
