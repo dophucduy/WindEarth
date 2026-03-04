cfd40685f93e2dac904c7000de16f43438e11082
THINH <thinhnguyen123258@gmail.com>
2026-03-04 20:56:02 +0700

    Modify movingplatform script in order to allow player moving together

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -39,4 +39,20 @@ public class PlatformScript : MonoBehaviour
             );
         }
     }
+    //Script de "Player" di chuyen theo platform
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
 }
